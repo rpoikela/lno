f18d07bcc02f8f5405420e662f9cbc5be047fcbd
Rauli Poikela <rauli.poikela@gmail.com>
2015-02-14 14:36:52 +0200

    Initial commit

---
+++ LNO/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LNO.Startup))]
+namespace LNO
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
